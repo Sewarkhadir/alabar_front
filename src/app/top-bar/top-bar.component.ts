c95e6d3e762e575bbee9c6e914f46b205a8c989d
skhadir <sewar.khadir@capgemini.com>
2023-05-24 12:24:07 +0200

    logout

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/auth/auth.service';
 
 @Component({
   selector: 'app-top-bar',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-bar.component.scss']
 })
 export class TopBarComponent implements OnInit {
-  constructor() {}
-  ngOnInit() {
-  }
+  constructor(private authService: AuthService) {}
+  ngOnInit() {}
+
   logout() {
-    //this.aS.logoutUser();
+    this.authService.logout();
   }
 }
